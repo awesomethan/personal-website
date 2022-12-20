593d570cac3f60dc2d9cb8efe11ee1ea51aaf2d8
awesomethan <89287311+awesomethan@users.noreply.github.com>
2022-12-20 15:02:49 -0500

    Update Project.js

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,18 +1,19 @@
 import React from "react";
-import Link from "react-router-dom";
 
-function Project({ title, description, image, codeLink, demoLink}) {
+function Project({ title, description, image, codeLink, demoLink }) {
   return (
     <div className="project-container">
       <div className="description-container">
-      <h1 className="heading">{title}</h1>
+        <h1 className="heading">{title}</h1>
         <div className="text portfolio-text">{description}</div>
-        <Link to={codeLink}>
-        <button className="portfolio-button text">VIEW CODE</button>
-        </Link>
-        <a href={demoLink}><button className="portfolio-button text">VIEW DEMO</button></a>
+        <a href={codeLink}>
+          <button className="portfolio-button text">VIEW CODE</button>
+        </a>
+        <a href={demoLink}>
+          <button className="portfolio-button text">VIEW DEMO</button>
+        </a>
       </div>
-      <img src={image} alt="" className="portfolio-img"/>
+      <img src={image} alt="" className="portfolio-img" />
     </div>
   );
 }
