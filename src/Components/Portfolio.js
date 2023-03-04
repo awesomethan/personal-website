f2d707dc4ee9cfbcc2fa79732070c76e54f3572f
Ethan Wang <ethanwang1203@gmail.com>
2023-03-03 23:39:16 -0500

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -18,7 +18,7 @@ function Portfolio() {
         projectLink={""}
       ></Project>
       <Project
-        date={"November 2022 - January 2023"}
+        date={"December 2022 - January 2023"}
         title={"Credictor"}
         description={
           "A site created in a team of 3 that uses machine learning trained on banking datasets to predict a person's eligibility for a credit card. I designed and built the front-end form and helped connect the React front-end with the Flask back-end."
