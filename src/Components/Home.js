7b45160a3c3b905e33b2887f17eea8133b22014b
Ethan Wang <ethanwang1203@gmail.com>
2024-10-14 13:20:32 -0400

    refactored code

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faSpotify,
+  // faSpotify,
   faLinkedin,
   faGithub,
-  faInstagram,
-  faDiscord,
+  // faInstagram,
+  // faDiscord,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
