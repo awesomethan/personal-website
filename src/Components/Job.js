176dde2b95afcb46915e910a3fd9ebac40d42b2d
Ethan Wang <ethanwang1203@gmail.com>
2024-11-07 17:17:35 -0500

    added phone screen responsiveness to experience page

--- src/Components/Job.js
+++ src/Components/Job.js
@@ -2,12 +2,16 @@ import React from "react";
 
 function Job({ title, date, company, image, skills }) {
   return (
-    <div className="experience-container">
-      <div className="experience-left">
+    <div className="experience-container flex flex-col md:flex-row">
+      <div className="flex flex-col md:flex-row justify-center items-center">
         <img src={image} alt="" className="experience-img" />
-        <div className="experience-text-container pl-12">
-          <h1 className="heading text-3xl py-6">{title}</h1>
-          <p className="text experience-subheading">{company}</p>
+        <div className="experience-text-container md:pl-12">
+          <h1 className="heading text-3xl py-6 text-center md:text-left">
+            {title}
+          </h1>
+          <p className="text experience-subheading text-center md:text-left">
+            {company}
+          </p>
           <div className="text experience-skills">
             <img src="images/skills.jpg" alt="" className="skills-img" />
             <p className="skills-text py-3">{skills}</p>
