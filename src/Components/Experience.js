da5acaea8eb5ccc336b67bc764b0e908edd2ef82
Ethan Wang <ethanwang1203@gmail.com>
2024-12-31 00:50:42 -0500

    update experience and fix mobile white space bug

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -10,21 +10,25 @@ function Experience() {
         date={"September - December 2024"}
         company={"WOLF Advanced Technology"}
         image={"images/wolflogo.jpg"}
-        skills={"C#, .NET, Git, SQL"}
+        skills={
+          "C#, .NET, EF Core, Git, Blazor, NUnit, Docker, Kubernetes, GitHub Actions"
+        }
       ></Job>
       <Job
         title={"Data Analyst"}
         date={"January - April 2024"}
         company={"Globalfaces Direct"}
         image={"images/gfdlogo.jpg"}
-        skills={"SQL, Python, Power BI, Power Query"}
+        skills={
+          "Python, SQL, Snowflake, Sigma Computing, Power BI, Power Query, Matillion"
+        }
       ></Job>
       <Job
         title={"QA Automation Engineer"}
         date={"May - August 2023"}
         company={"Globalfaces Direct"}
         image={"images/gfdlogo.jpg"}
-        skills={"SQL, VBA, Microsoft Excel, Google Apps Script"}
+        skills={"SQL, SSMS, VBA, Google Apps Script, Excel"}
       ></Job>
     </div>
   );
